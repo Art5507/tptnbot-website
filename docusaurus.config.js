3b7d5eb38b47d815a1448af1c47a5313f65b1472
Art5507 <arturbakiev028@gmail.com>
2024-08-02 20:32:20 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Топтуновый бот',
-  tagline: 'Еще один Discord бот',
+  tagline: 'Еще один чат-бот',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
